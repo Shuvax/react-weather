74c7751a8c9feff90f5e41c8ed74d5790a5353da
Shuva <34592080+Shuvax@users.noreply.github.com>
2020-09-29 15:18:14 -0400

    Updates

---
+++ precache-manifest.a9cb7429a1baaed577b67d88b72b221a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4f7e688c38a415a87b6721747173f7",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "149f68d306775787a48d",
+    "url": "/react-weather/static/css/main.e6ac3243.chunk.css"
+  },
+  {
+    "revision": "d9ac85e274391cd42e7f",
+    "url": "/react-weather/static/js/2.1dda5cd3.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/react-weather/static/js/2.1dda5cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "149f68d306775787a48d",
+    "url": "/react-weather/static/js/main.98f64be6.chunk.js"
+  },
+  {
+    "revision": "f62618312724e8c367b8",
+    "url": "/react-weather/static/js/runtime-main.ee67f24b.js"
+  }
+]);
